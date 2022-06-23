5ba42522e18341d66ce3b46a0d976e5a9745d6bf
Nathaniel Forsyth <ironsideforsyth@gmail.com>
2022-06-23 15:29:48 +0100

    All extenstion complete

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from modules.output import *
 from modules.task_list import *
-
+from data.task_list import *
+from modules.input import *
 
 while (True):
     print_menu()
-    option = input("Select an option 1, 2, 3, 4, 5, display (m)enu or (q)uit: ")
+    option = user_option_selection()
+    # option = input("Select an option 1, 2, 3, 4, 5, display (m)enu or (q)uit: ")
     if (option.lower() == 'q'):
         break
     if option == '1':
@@ -14,23 +16,23 @@ while (True):
     elif option == '3':
         print_list(get_completed_tasks(tasks))
     elif option == '4':
-        description = input("Enter task description to search for: ")
-        task = get_task_with_description(tasks, description)
+        # description = input("Enter task description to search for: ")
+        task = get_task_with_description(tasks, user_description_input())
         if task is not None:
             mark_task_complete(task)
             print("Task marked complete")
         else:
             print("Task not found")
     elif option == '5':
-        time = int(input("Enter task duration: "))
-        print_list(get_tasks_taking_at_least(tasks, time))
+        # time = int(input("Enter task duration: "))
+        print_list(get_tasks_taking_at_least(tasks, user_time_input()))
     elif option == '6':
-        description = input("Enter task description to search for: ")
-        print(get_task_with_description(tasks, description))
+        # description = input("Enter task description to search for: ")
+        print(get_task_with_description(tasks, user_description_input()))
     elif option == '7':
-        description = input("Enter description: ")
-        time_taken = int(input("Enter time taken: "))
-        task = create_task(description, time_taken)
+        # description = input("Enter description: ")
+        # time_taken = int(input("Enter time taken: "))
+        task = create_task(user_description_input(), user_time_input())
         tasks.append(task)
     else:
         print("Invalid Input - choose another option")
---
+++ modules/input.py
@@ -0,0 +1,14 @@
+def user_option_selection():
+    option = input("Select an option 1, 2, 3, 4, 5, display (m)enu or (q)uit: ")
+    return option
+
+
+def user_description_input():
+    description = input("Enter task description to search for: ")
+    return description
+
+def user_time_input():
+    time = int(input("Enter task duration: "))
+    return time
+
+
